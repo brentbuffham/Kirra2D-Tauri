e4aec92b65a2677e28b04181f47d681a671aa424
brentbuffham <brent.buffham@gmail.com>
2025-07-13 13:42:49 +0800

    Tauri Update Attempt mostly working but the save buttons are not and niether is the print output.

--- Kirra2D/src-tauri/src/lib.rs
+++ Kirra2D/src-tauri/src/lib.rs
@@ -1,20 +1,25 @@
-// use tauri::{CustomMenuItem, Menu, Submenu};
+ use tauri::{command, generate_handler, Builder};
 
-#[tauri::command]
+#[command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-//#[cfg_attr(mobile, tauri::mobile_entry_point)]
+#[command]
+fn close_app() {
+    println!("🚪 Force quitting application");
+    std::process::exit(0);
+}
+
 pub fn run() {
-    tauri::Builder::default()
+    Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(generate_handler![greet, close_app])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
